14c51921bd90a639a733db0c8d17533d64b4d097
VapormaxGYP <vapormaxgyp@gmail.com>
2022-08-08 01:08:55 +0800

    Modify Exception Handler

--- src/main/java/com/vapor/eshop/exception/ExceptHandler.java
+++ src/main/java/com/vapor/eshop/exception/ExceptHandler.java
@@ -23,7 +23,7 @@ public class ExceptHandler {
 
     @ExceptionHandler(Exception.class)
     public Result<?> ExceptionHandler(Exception e){
-        //generate exception second
+        //generate exception second gg
         Result<Object> result = new Result<>();
         result.setCode(501);
         result.setMsg("System Error");
