e671fbed0eeb81b84201a57f6a7dc0af5f6c88b0
VapormaxGYP <vapormaxgyp@gmail.com>
2022-08-12 20:36:00 +0800

    Get Uer Detail Info

--- src/main/java/com/vapor/eshop/allconfig/CorsConfig.java
+++ src/main/java/com/vapor/eshop/allconfig/CorsConfig.java
@@ -5,25 +5,29 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * Cors Config
  */
 
-/*
 @Configuration
 
 public class CorsConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/**")
-                .allowedOriginPatterns("*")
-                .allowCredentials(true)
-                .allowedMethods("*")
-                .maxAge(3600);
+    @Bean
+    public CorsFilter corsFilter(){
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedHeader(CorsConfiguration.ALL);
+        config.addAllowedMethod(CorsConfiguration.ALL);
+        config.addAllowedOrigin(CorsConfiguration.ALL);
+        config.addExposedHeader(CorsConfiguration.ALL);
+        config.addAllowedOriginPattern(CorsConfiguration.ALL);
+        //config.setAllowCredentials(true);
+
+
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(urlBasedCorsConfigurationSource);
     }
 }
-
- */
