7acbe5e13b249c9f092908b81c2431475df1081c
VapormaxGYP <vapormaxgyp@gmail.com>
2022-08-16 03:00:45 +0800

    Add SSL config

--- src/main/java/com/vapor/eshop/allconfig/HttpsConfig.java
+++ src/main/java/com/vapor/eshop/allconfig/HttpsConfig.java
@@ -1,9 +1,48 @@
 package com.vapor.eshop.allconfig;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
 @Configuration
 public class HttpsConfig {
+    @Value("${http.port}")
+    private Integer httpPort;
+
+    @Value("${server.port}")
+    private Integer httpsPort;
+    @Bean
+    public ServletWebServerFactory servletContainer(){
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory(){
+            protected void postProcessContext(Context context){
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("confidential");
+                SecurityCollection securityCollection = new SecurityCollection();
+                securityCollection.addPattern("/**");
+                securityConstraint.addCollection(securityCollection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(HttpToSSL());
+        return tomcat;
+    }
+
+    private Connector HttpToSSL(){
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(httpPort);
+        connector.setSecure(false);
+        connector.setRedirectPort(httpsPort);
+        return connector;
+    }
 
 }
